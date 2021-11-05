86c9d2384caae1b4879c4fa239ee2252caca67a7
Ashka4 <a.kntr93@gmail.com>
2021-11-05 22:57:08 +0300

    Add quasar framework

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,7 +9,7 @@
 }
 
 #app {
-  font-family: Arial, Helvetica, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -6,8 +6,13 @@ export default {
 
 <template>
   <div class="navbar">
-    <router-link to="/" class="logo"><img src="../assets/logo.png"></router-link>
-    <router-link to="/login" class="login">Login</router-link>
+    <div class="logo">
+      <router-link to="/" class="navbar_logo"><img src="../assets/logo.png"></router-link>
+    </div>
+    <div class="navbar_buttons">
+      <q-btn class="start" to="/start" label="Get Started For Free" rounded color="green" size="18px" />
+      <q-btn class="login" to="/login" label="Log In" outline color="black" size="18px" />
+    </div>
   </div>
 </template>
 
@@ -16,13 +21,16 @@ export default {
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: space-between;
+  justify-content: space-around;
+  margin-top: 20px;
 }
 
-.navbar img {
-  width: 70%;
+.navbar_logo img {
+  width: 150px;
 }
-.login {
-  padding: 20px;
+
+.navbar_buttons .start {
+  margin-right: 20px;
 }
+
 </style>
