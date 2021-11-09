54f79654765a110c486585e2941c8a9be6fcb6ce
Ashka4 <a.kntr93@gmail.com>
2021-11-09 18:12:17 +0300

    Add ApexCharts

--- frontend/src/views/Calorie.vue
+++ frontend/src/views/Calorie.vue
@@ -1,10 +1,11 @@
 <script>
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import AreaChart from '@/components/AreaChart'
 
 export default {
   name: 'Calorie',
-  components: { Navbar, Footer },
+  components: { Navbar, Footer, AreaChart },
   data () {
     return {
       options: [
@@ -26,16 +27,24 @@ export default {
     </div>
     <div class="calorie_body">
       <div class="inputs">
-        <div class="title">
-          <h3>CALORIE CALCULATOR</h3>
+        <div class="imperial">
+          <div class="imperial_title">
+            <h5>imperial</h5>
+          </div>
+          <div class="imperial_inputs">
+            <q-input class="lbs" ref="lbsRef" filled type="number" v-model="lbs" label="lbs" lazy-rules :rules="lbsRules" color="black" />
+            <q-input class="ft" ref="ftRef" filled type="number" v-model="ft" label="ft" lazy-rules :rules="ftRules" color="black" />
+            <q-input class="in" ref="inRef" filled type="number" v-model="inc" label="in" lazy-rules :rules="inRules" color="black" />
+          </div>
         </div>
-        <div class="weight">
-          <q-input class="kg" ref="kgRef" filled type="number" v-model="kg" label="kg" lazy-rules :rules="kgRules" color="black" />
-          <q-input class="lbs" ref="lbsRef" filled type="number" v-model="lbs" label="lbs" lazy-rules :rules="lbsRules" color="black" />
-        </div>
-        <div class="height">
-          <q-input class="cm" ref="cmRef" filled type="number" v-model="cm" label="cm" lazy-rules :rules="cmRules" color="black" />
-          <q-input class="in" ref="inRef" filled type="number" v-model="inc" label="in" lazy-rules :rules="inRules" color="black" />
+        <div class="metric">
+          <div class="metric_title">
+            <h5>metric</h5>
+          </div>
+          <div class="metric_inputs">
+            <q-input class="kg" ref="kgRef" filled type="number" v-model="kg" label="kg" lazy-rules :rules="kgRules" color="black" />
+            <q-input class="cm" ref="cmRef" filled type="number" v-model="cm" label="cm" lazy-rules :rules="cmRules" color="black" />
+          </div>
         </div>
         <div class="select_age">
           <q-input  class="age" ref="ageRef" filled type="number" v-model="age" label="Age" lazy-rules :rules="ageRules" color="black" />
@@ -57,12 +66,12 @@ export default {
           ]"/>
         </div>
         <div class="button">
-          <q-btn push class="done" color="black" label="CALCULATE" size="25px"/>
+          <q-btn push class="done" color="black" label="CALCULATE" size="22px"/>
         </div>
       </div>
-      <div class="chart_results">
+      <div v-if="true" class="chart_results">
         <div class="chart">
-          <h1>CHART</h1>
+          <AreaChart />
         </div>
         <div class="results">
           <div class="bmr">
@@ -76,7 +85,7 @@ export default {
         </div>
       </div>
     </div>
-    <q-banner class="bg-blue-1" >
+    <q-banner class="info" >
       <div class="bmr_info">
         <h4>WHAT IS BMR?</h4>
         <p>
@@ -100,81 +109,71 @@ export default {
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Mochiy+Pop+One&family=Roboto+Slab:wght@500;900&display=swap');
 
+.calorie .calorie_body .inputs {
+  padding: 40px;
+  box-shadow: rgba(0, 0, 0, 0.5) 0px 2px 5px, rgba(0, 0, 0, 0.05) 0px 2px 15px;
+}
+
 .calorie .calorie_body {
   display: flex;
   flex-direction: row;
-  justify-content:space-evenly;
-  margin-top: 5%;
+  justify-content:center;
+  gap: 40px;
+  margin-top: 3%;
 }
 
 .calorie .chart_results {
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
+  align-content: center;
 }
 
 .calorie .calorie_body .inputs {
   display: flex;
   flex-direction: column;
-  gap: 25px;
+  gap: 30px;
 }
 
-.calorie .calorie_body .inputs .title h3 {
-  font-weight: bolder;
-  font-size: 32px;
-  font-family: sans-serif;
-  margin-bottom: 5%;
-  background: #000000;
-  color: rgb(255, 255, 255);
-  margin-bottom: 15px;
-  padding: 10px;
-  border-radius: 15px;
+.calorie .calorie_body .inputs .metric{
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 }
 
-.calorie .calorie_body .inputs .weight{
+.calorie .calorie_body .inputs .imperial h5 {
+  font-family: 'Roboto Slab', serif;
+  font-size: 20px;
+}
+
+.calorie .calorie_body .inputs .metric h5 {
+  font-family: 'Roboto Slab', serif;
+  font-size: 20px;
+}
+
+.calorie .calorie_body .inputs .imperial{
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+}
+
+.calorie .calorie_body .inputs .metric .metric_inputs {
   display: flex;
   flex-direction: row;
-  gap: 50px;
+  gap: 20px;
 }
 
-.calorie .calorie_body .inputs .height{
+.calorie .calorie_body .inputs .imperial .imperial_inputs {
   display: flex;
   flex-direction: row;
-  gap: 50px;
-}
-
-.calorie .calorie_body .inputs .weight .lbs {
-  border: 1px solid rgb(100, 100, 100);
-}
-.calorie .calorie_body .inputs .weight .kg {
-  border: 1px solid rgb(100, 100, 100);
-}
-.calorie .calorie_body .inputs .select_age .age {
-  border: 1px solid rgb(100, 100, 100);
-}
-.calorie .calorie_body .inputs .height .cm {
-  border: 1px solid rgb(100, 100, 100);
-}
-.calorie .calorie_body .inputs .height .in {
-  border: 1px solid rgb(100, 100, 100);
-}
-
-.calorie .calorie_body .inputs .weight{
-  display: flex;
-  flex-direction: row;
-  gap: 50px;
+  gap: 20px;
 }
 
 .calorie .calorie_body .inputs .button {
   display: flex;
   justify-content: center;
-  margin-top: 15%;
-  margin-bottom: 30%;
-}
-
-.calorie .calorie_body .inputs .gender {
-  display: flex;
-  justify-content: center;
+  margin-top: 5%;
 }
 
 .calorie .results {
@@ -186,32 +185,31 @@ export default {
   justify-content: center;
   margin-top: 5%;
   gap: 30px;
-  box-shadow: rgba(0, 0, 0, 0.5) 0px 2px 5px, rgba(0, 0, 0, 0.05) 0px 2px 15px;
-  padding: 20px;
   font-family: 'Roboto Slab', serif;
 }
 
 .calorie .chart_results .chart {
-  margin-bottom: 55%;
+  margin-bottom: 10%;
 }
 
-.calorie .bg-blue-1 {
+.calorie .info {
   display: flex;
   flex-direction: column;
   gap: 20px;
   border-radius: 30px;
   margin-left: 20%;
   margin-right: 20%;
+  margin-top: 6%;
   margin-bottom: 3%;
   font-family: 'Roboto Slab', serif;
 }
 
-.calorie .bg-blue-1 .bmr_info{
+.calorie .info .bmr_info{
   border-bottom:10px solid rgb(255, 255, 0);
   margin-bottom: 1%;
 }
 
-.calorie .bg-blue-1 p {
+.calorie .info p {
   font-size: 17px;
   margin-top: 1%;
 }
