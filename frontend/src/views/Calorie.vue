fbdceb251432b25bf7be8b96b4ff4824b21d6862
Ashka4 <a.kntr93@gmail.com>
2021-11-09 15:39:10 +0300

    Update calorie component

--- frontend/src/views/Calorie.vue
+++ frontend/src/views/Calorie.vue
@@ -27,7 +27,7 @@ export default {
     <div class="calorie_body">
       <div class="inputs">
         <div class="title">
-          <h3>Calorie Calculator</h3>
+          <h3>CALORIE CALCULATOR</h3>
         </div>
         <div class="weight">
           <q-input class="kg" ref="kgRef" filled type="number" v-model="kg" label="kg" lazy-rules :rules="kgRules" color="black" />
@@ -60,17 +60,36 @@ export default {
           <q-btn push class="done" color="black" label="CALCULATE" size="25px"/>
         </div>
       </div>
-    </div>
-      <div class="results">
-        <div>
-          <h5>Your BMR is</h5>
-          <h4>2000 kcal</h4>
+      <div class="chart_results">
+        <div class="chart">
+          <h1>CHART</h1>
         </div>
-        <div>
-          <h5>Your TDD is</h5>
-          <h4>2400 kcal</h4>
+        <div class="results">
+          <div class="bmr">
+            <h5>Your BMR is</h5>
+            <h4>2000 kcal</h4>
+          </div>
+          <div class="tdee">
+            <h5>Your TDEE is</h5>
+            <h4>2400 kcal</h4>
+          </div>
         </div>
       </div>
+    </div>
+    <q-banner class="bg-blue-1" >
+      <div class="bmr_info">
+        <h4>WHAT IS BMR?</h4>
+        <p>
+        Basal Metabolic Rate (BMR) is how many calories you burn when your body is resting.BMR is the basic number of calories you need to sustain life. It’s the energy your body needs to keep your heart pumping, your circulation, lungs, brain, and other vital organs working, and your body temperature regulated.
+        </p>
+      </div>
+      <div class="tdee_info">
+        <h4>WHAT IS TDEE?</h4>
+        <p>
+        Total Daily Energy Expenditure (TDEE) is an estimation of how many calories you burn each day, including physical activity. It is calculated by multiplying your Basal Metabolic Rate (BMR) by 1.2 - 1.9 based on your activity level.
+        </p>
+      </div>
+    </q-banner>
     <div class="calorie_image"> </div>
     <div class="calorie_footer">
       <Footer />
@@ -83,8 +102,15 @@ export default {
 
 .calorie .calorie_body {
   display: flex;
-  justify-content: center;
+  flex-direction: row;
+  justify-content:space-evenly;
   margin-top: 5%;
+}
+
+.calorie .chart_results {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
 .calorie .calorie_body .inputs {
@@ -95,12 +121,14 @@ export default {
 
 .calorie .calorie_body .inputs .title h3 {
   font-weight: bolder;
-  font-size: 40px;
-  font-family: 'Roboto Slab', serif;
+  font-size: 32px;
+  font-family: sans-serif;
   margin-bottom: 5%;
-  border-radius: 20px;
-  padding: 20px;
-  background: linear-gradient(100deg, #faffad 0%, #ffe341 20%, #fff78b 40%, #e9ff1e 60%, #fbff1e 80%);
+  background: #000000;
+  color: rgb(255, 255, 255);
+  margin-bottom: 15px;
+  padding: 10px;
+  border-radius: 15px;
 }
 
 .calorie .calorie_body .inputs .weight{
@@ -117,29 +145,31 @@ export default {
 
 .calorie .calorie_body .inputs .weight .lbs {
   border: 1px solid rgb(100, 100, 100);
-  border-radius: 10px;
 }
 .calorie .calorie_body .inputs .weight .kg {
   border: 1px solid rgb(100, 100, 100);
-  border-radius: 10px;
 }
 .calorie .calorie_body .inputs .select_age .age {
   border: 1px solid rgb(100, 100, 100);
-  border-radius: 10px;
 }
 .calorie .calorie_body .inputs .height .cm {
   border: 1px solid rgb(100, 100, 100);
-  border-radius: 10px;
 }
 .calorie .calorie_body .inputs .height .in {
   border: 1px solid rgb(100, 100, 100);
-  border-radius: 10px;
+}
+
+.calorie .calorie_body .inputs .weight{
+  display: flex;
+  flex-direction: row;
+  gap: 50px;
 }
 
 .calorie .calorie_body .inputs .button {
   display: flex;
   justify-content: center;
   margin-top: 15%;
+  margin-bottom: 30%;
 }
 
 .calorie .calorie_body .inputs .gender {
@@ -155,13 +185,53 @@ export default {
   align-items: center;
   justify-content: center;
   margin-top: 5%;
-  gap: 15px;
-  margin-bottom: 5%;
+  gap: 30px;
+  box-shadow: rgba(0, 0, 0, 0.5) 0px 2px 5px, rgba(0, 0, 0, 0.05) 0px 2px 15px;
+  padding: 20px;
+  font-family: 'Roboto Slab', serif;
 }
 
-.calorie .results h4 {
+.calorie .chart_results .chart {
+  margin-bottom: 55%;
+}
+
+.calorie .bg-blue-1 {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  border-radius: 30px;
+  margin-left: 20%;
+  margin-right: 20%;
+  margin-bottom: 3%;
+  font-family: 'Roboto Slab', serif;
+}
+
+.calorie .bg-blue-1 .bmr_info{
+  border-bottom:10px solid rgb(255, 255, 0);
+  margin-bottom: 1%;
+}
+
+.calorie .bg-blue-1 p {
+  font-size: 17px;
+  margin-top: 1%;
+}
+
+.calorie .chart_results .results .bmr {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 20px;
+}
+
+.calorie .chart_results .results .tdee {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 20px;
+}
+
+.calorie .chart_results .results h4 {
   font-weight: bold;
-  margin-bottom: 5px;
 }
 
 .calorie .calorie_image {
