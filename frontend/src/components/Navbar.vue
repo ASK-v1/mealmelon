903d08eb7a4555fc148fc56a7db68e5ff9ce9cff
Ashka4 <a.kntr93@gmail.com>
2021-11-23 13:36:26 +0300

    Update Navbar.vue

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -10,8 +10,7 @@ export default {
       <router-link to="/" class="navbar_logo"><img src="../assets/logo.png"></router-link>
     </div>
     <div class="navbar_buttons">
-      <q-btn class="start" to="/start" label="Get Started For Free" color="black" size="18px" />
-      <q-btn class="login" to="/login" label="Log In" outline color="black" size="18px" />
+      <q-btn class="start" to="/calorie" label="Get Started For Free" outline color="black" size="20px" />
     </div>
   </div>
 </template>
@@ -21,7 +20,7 @@ export default {
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: space-around;
+  justify-content: space-evenly;
   margin-top: 10px;
 }
 
@@ -30,26 +29,6 @@ export default {
 }
 
 .navbar_buttons .start {
-  margin-right: 25px;
-  font-weight: 700;
-}
-
-.navbar_buttons .login {
-  font-weight: 700;
-}
-
-@media (max-width: 900px) {
-  .navbar {
-    justify-content: center;
-  }
-  .navbar_logo img  {
-    justify-content: center;
-  }
-  .navbar_buttons .start {
-    display:none;
-  }
-  .navbar_buttons .login {
-    width:  100px;
-  }
+  font-weight: bold;
 }
 </style>
